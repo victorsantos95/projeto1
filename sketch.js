e08806ec35c2f3d5cb70a861a91b145ec79681fa
victorsantos95 <00001118650256sp@al.educacao.sp.gov.br>
2024-07-31 14:53:36 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(1000, 800);
+  background("black")
+}
+
+
+function draw() {
+  
+
+  fill("rgb(255,0,0)");
+  stroke("lightblue")
+  
+  if (mouseIsPressed);{
+  rect(mouseX,mouseY,90,90);
+  }
+}
